069497eb9054524ce6831f5ae8d58aa943776f14
jamesChen <702660780@qq.com>
2021-01-12 23:00:08 +0800

    add 20210112

--- 13、分治法 二分法/螺纹螺帽匹配问题.py
+++ 13、分治法 二分法/螺纹螺帽匹配问题.py
@@ -12,15 +12,46 @@
     B1. 比螺母小的螺丝
     B2. 比螺母大的螺丝
     A2跟B1一一对应，A3跟B2一一对应，对(A2,B1)和(A3,B2)分别执行上述的算法，直至完全匹配。
+    a=[4,5,2,6,1,7,8,9,3]
+    b=[6,2,1,8,9,7,4,5,3]
 '''
 
 class Solution:
-
+    def __init__(self):
+        self.n=1
     def match(self,a,b):
         self.sort(a,b,0,len(a)-1)
+        print(a)
+        print(b)
 
     def sort(self,a,b,left,right):
-        pass
+        if left<right:
+            temp=a[left] #用a来区分b
+            i = left
+            j = right
+            while i<j:
+                while i<j and b[i]<temp:
+                    i=i+1
+                while i<j and b[j]>temp:
+                    j=j-1
+                b[i],b[j]=b[j],b[i]
+            b[i],b[left]=b[left],b[i]
+            print("滴{}次操作 b:{}".format(self.n,b))
+            temp=b[left+1] #用b来区分a
+            i = left + 1
+            j = right
+            while i<j:
+                while i<j and a[i]<temp:
+                    i=i+1
+                while i<j and a[j]>temp:
+                    j=j-1
+                a[i], a[j] = a[j], a[i]
+            a[left+1],a[i]=a[i],a[left+1]
+            print("滴{}次操作 a:{}".format(self.n, a))
+            self.n=self.n+1
+            self.sort(a,b,left+2,i)
+            self.sort(a,b,i+1,right)
+
 
 if __name__ == '__main__':
     s=Solution()
